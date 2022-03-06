ef6bf1ef471ca495cc082a8226318ce4a7b72cbd
thomaskost17 <thomaskost17@ucla.edu>
2022-03-06 14:13:56 -0800

    solver and rnn object and lr decay

--- nn/rnn.py
+++ nn/rnn.py
@@ -8,22 +8,33 @@
   @brief This file will define our RNN object
 '''
 import torch, torchaudio, torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable 
 
-class RNN():
-    def __init__(self):
-        '''
-           @breif 
-           @return 
-           @param 
-           @param 
-        '''
+class RNN(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(RNN, self).__init__()
+        self.num_classes = num_classes #number of classes
+        self.num_layers = num_layers #number of layers
+        self.input_size = input_size #input size
+        self.hidden_size = hidden_size #hidden state
+        
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                          num_layers=num_layers, batch_first=True) #lstm
+        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
+        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+        self.relu = nn.ReLU()
     
-        pass
-    def train(self):
-        '''
-        @breif 
-        @return 
-        @param 
-        @param 
-        '''
-        pass
+    def forward(self,x):
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
+        # Propagate input through LSTM
+        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        out = self.relu(hn)
+        out = self.fc_1(out) #first Dense
+        out = self.relu(out) #relu
+        out = self.fc(out) #Final Output
+        return out
